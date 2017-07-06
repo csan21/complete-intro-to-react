2770e47b7aee0d71181af15b9dbe74e97fef76f7
csan21 <chrisswho@gmail.com>
2017-07-06 16:29:33 -0500

    Test out prettier for formatting

---
+++ js/testapp.jsx
@@ -0,0 +1,15 @@
+const MyTitle = function(props) {
+  return React.createElement('div', null, React.createElement('h1', { style: { color: props.color } }, props.title));
+};
+
+const MyFirstComponent = function() {
+  return React.createElement(
+    'div',
+    { id: 'first-component' },
+    React.createElement(MyTitle, { title: 'Test 1', color: 'lightsteelblue' }),
+    React.createElement(MyTitle, { title: 'Test 2', color: 'tomato' }),
+    React.createElement(MyTitle, { title: 'Test 3', color: 'lightslategrey' })
+  );
+};
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'));
