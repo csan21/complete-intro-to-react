bb602d41a26b583875efb4bd28422cd163118970
csan21 <chrisswho@gmail.com>
2017-07-06 22:18:46 -0500

    Add babel and convert to jsx, DAY 1 over

--- js/testapp.jsx
+++ js/testapp.jsx
@@ -2,24 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 
 const MyTitle = function(props) {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement('h1', { style: { color: props.color } }, props.title)
+  return (
+    <div>
+      <h1 style={{color: props.color}}>{props.title}</h1>
+    </div>
   );
 };
 
 const MyFirstComponent = function() {
-  return React.createElement(
-    'div',
-    { id: 'first-component' },
-    React.createElement(MyTitle, { title: 'Test 1', color: 'lightsteelblue' }),
-    React.createElement(MyTitle, { title: 'Test 2', color: 'tomato' }),
-    React.createElement(MyTitle, { title: 'Test 3', color: 'lightslategrey' })
+  return (
+    <div id="first-component">
+      <MyTitle title="Test 1" color="lightsteelblue" />
+      <MyTitle title="Test 2" color="tomato" />
+      <MyTitle title="Test 3" color="lightslategrey" />
+    </div>
   );
 };
 
-ReactDOM.render(
-  React.createElement(MyFirstComponent),
-  document.getElementById('app')
-);
+render(<MyFirstComponent />, document.getElementById('app'))
