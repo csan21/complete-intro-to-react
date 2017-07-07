7095d0049cefe4747b3f825783dc82cccd1cc04d
csan21 <chrisswho@gmail.com>
2017-07-07 11:57:14 -0500

    Webpack dev server working

--- js/testapp.jsx
+++ js/testapp.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const MyTitle = function(props) {
-  return (
-    <div>
-      <h1 style={{color: props.color}}>{props.title}</h1>
+const App = () => (
+  <div className='app'>
+    <div className='landing'>
+      <h1>svideo</h1>
+      <input type='text' placeholder='Search' />
+      <a>or Browse All</a>
     </div>
-  );
-};
+  </div>
+);
 
-const MyFirstComponent = function() {
-  return (
-    <div id="first-component">
-      <MyTitle title="Test 1" color="lightsteelblue" />
-      <MyTitle title="Test 2" color="tomato" />
-      <MyTitle title="Test 3" color="lightslategrey" />
-    </div>
-  );
-};
-
-render(<MyFirstComponent />, document.getElementById('app'))
+render(<App />, document.getElementById('app'));
