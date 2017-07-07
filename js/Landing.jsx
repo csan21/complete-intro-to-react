4afe87b594ae023f9b74044770e924684b4251ef
csan21 <chrisswho@gmail.com>
2017-07-07 12:35:33 -0500

    JSON data

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Landing = () => (
-  <div className='landing'>
+  <div className="landing">
     <h1>svideo</h1>
-    <input type='text' placeholder='Search' />
+    <input type="text" placeholder="Search" />
     <Link to="/search">or Browse All</Link>
   </div>
 );
