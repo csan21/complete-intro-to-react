8fafebae444fbcc56a2db55d8de0ec6fa782f3d9
csan21 <chrisswho@gmail.com>
2017-07-14 14:34:56 -0500

    Test out match

--- js/Details.jsx
+++ js/Details.jsx
@@ -5,7 +5,6 @@ import React from 'react';
 const Details = props => (
   <div className="details">
     <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
-    <h1>test lol</h1>
   </div>
 );
 
