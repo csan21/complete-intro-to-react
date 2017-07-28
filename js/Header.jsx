a1d991be43701ea472f973411fa0bc7f424b382b
csan21 <chrisswho@gmail.com>
2017-07-28 15:28:36 -0500

    Add header component

---
+++ js/Header.jsx
@@ -0,0 +1,45 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props: {
+  showSearch?: boolean,
+  handleSearchTermChange?: Function,
+  searchTerm?: string
+}) => {
+  let utilSpace;
+  if (props.showSearch) {
+    utilSpace = (
+      <input
+        onChange={this.handleSearchTermChange}
+        value={this.state.searchTerm}
+        type="text"
+        placeholder="Search"
+      />
+    );
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to="/search">
+          Back
+        </Link>
+      </h2>
+    );
+  }
+  <header>
+    <h1>
+      <Link to="/">
+        svideo
+      </Link>
+    </h1>
+  </header>;
+};
+
+Header.defaultProps = {
+  showSearch: false,
+  handleSearchTermChange: function noop() {},
+  searchTerm: ''
+};
+
+export default Header;
